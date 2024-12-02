46bbeab50d9fe1fb72da7d9493c3c23c1fda42da
priyanshu <your.email@example.com>
2024-12-02 14:08:34 +0530

    add refreshToken endpoint

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,15 +3,18 @@ import { apiError } from "../utils/apiError.js";
 import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { apiResponce } from "../utils/apiResponce.js";
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefreshTokens = async(userId) => {
   try {
   const user =  await User.findById(userId)
- const assessToken  = user.generateAccessToken()
+ const accessToken  = user.generateAccessToken()
  const refreshToken = user.generateRefreshToken()
 
 user.refreshToken = refreshToken
  await user.save({validateBeforeSave: false})
+
+ return {accessToken, refreshToken}
 
 
   } catch (error) {
@@ -101,7 +104,7 @@ const loginUser = asyncHandler(async (req, res ) =>{
 
     const {email, username, password} = req.body
 
-    if (!username || !email) {
+    if (!username && !email) {
       throw new apiError(400, "username or password is required")
     }
 
@@ -119,10 +122,109 @@ const loginUser = asyncHandler(async (req, res ) =>{
     throw new apiError(401," Invalid user credentials")
     
   }
+
+  const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+ const loggedInUser  = await User.findById(user._id).
+ select("-password -refreshToken")
+
+ const options = {
+  httpOnly: true,
+  secure: true
+ }
+
+ return res
+ .status(200)
+ .cookie("accessToken", accessToken, options) // Set access token
+ .cookie("refreshToken", refreshToken, options) // Set refresh token
+ .json(
+   new apiResponce(
+     200,
+     {
+       user: loggedInUser,
+       accessToken,
+       refreshToken
+     },
+     "User logged in successfully"
+   )
+ );
+
+})
+
+const logoutUser = asyncHandler(async(req, res) => {
+   await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined
+      }
+    },
+    {
+      new: true
+    }
+   )
+   const options = {
+    httpOnly: true,
+    secure: true
+   }
+
+   return res.status(200)
+   .clearCookie("accessToken", options)
+  .clearCookie("refreshToken", options)
+  .json(new apiResponce(200, {}, "User logged Out"))
+})
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshAccessToken
+
+  if (!incomingRefreshToken) {
+    throw new apiError(401, "Unauthorized request")
+  }
+
+ try {
+   const decodedToken = jwt.verify(
+     incomingRefreshToken,
+     process.env.REFRESH_TOKEN_SECRET
+   )
+ 
+  const user = await User.findById(decodedToken?._id)
+ 
+  if (!user) {
+   throw new apiError(401, "Invalid refresh token")
+  }
+ 
+  if (incomingRefreshToken !== user?.refreshToken) {
+   throw new apiError(401, "Refresh token is expired or used")
+  }
+ 
+  const options = {
+   httpOnly: true,
+   secure: true
+  }
+ 
+ const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+ 
+  return res
+  .status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", newRefreshToken, options)
+  .json(
+   new apiResponce(
+     200,
+   {accessToken, refreshToken: newRefreshToken},
+   "Access token refreshed"
+   )
+  )
+ 
+ } catch (error) {
+  throw new apiError(401, error?.message || "Invalid refresh token")
+ }
 })
 
 
 export {
   registerUser,
-  loginUser
+  loginUser,
+  logoutUser,
+  refreshAccessToken
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,22 +1,32 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js"
-import {upload} from "../middlewares/multer.middlewares.js"
+import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middlewares.js";
+import pkg from "jsonwebtoken"; // Import jsonwebtoken as a default import
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
+const { verify } = pkg; // Destructure the `verify` method from jsonwebtoken
 
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        }, 
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
-)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-export default router
+router.route("/login").post(loginUser)
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
+
+export default router;
